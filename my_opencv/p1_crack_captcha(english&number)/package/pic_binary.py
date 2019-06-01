ed4852de2240ca52aa345e2e7ee6b80e19f544ae
wikensmith <302856076@qq.com>
2019-06-01 22:01:27 +0800

    gen_captcha is complete.

--- my_opencv/p1_crack_captcha(english&number)/package/pic_binary.py
+++ my_opencv/p1_crack_captcha(english&number)/package/pic_binary.py
@@ -10,29 +10,19 @@ import time
 
 
 class PicBinary:
-    def __init__(self, img):
-        self.img = img
+    def __init__(self):
         self.q = Queue()
         self.visited = set()
         self.binaryed_img = ""
+        self.i = 0
+        self.blured = ""
+        self.text = ""
 
     def binary(self):
         gray = cv.cvtColor(self.img, cv.COLOR_BGR2GRAY)
-        # gaussican = cv.GaussianBlur(gray, (3,3), 3,0)
         ret, binary = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
         blured = cv.medianBlur(binary, 5)
-        blured2 = cv.medianBlur(binary, 5)
-        # kernel = np.ones((3,3), np.uint8)
-        # opening = cv.morphologyEx(blured, cv.MORPH_OPEN, kernel)
-        # cv.imshow("img", self.img)
-        # cv.imshow("bin", opening)
-        # cv.imshow("blured", blured)
-        # cv.imshow("blured2", blured2)
-        # print(blured2.shape, "shape")
-        # name = f'./pic/dealed/{str(int(time.time()*1000))}_{text}.png'
-        # cv.imwrite(name, blured2)
-        # cv.waitKey(0)
-        return blured2
+        return blured
 
     def delete_noisy(self, img):
         h, w = img.shape
@@ -42,7 +32,33 @@ class PicBinary:
                 (-1, -1), (-1, 2),
                 (-2, -1), (-2, 0), (-2, 1), (-2, 2)]
         # func  = [(1, -1),(1, 0),(1, 1),(0, -1),(0, 1),(-1, -1),(-1, 0),(-1, 1)]
+        func = [(1, -1), (1, 0), (1, 1), (1, 2), (1, 3),
+                (0, -1), (0, 3),
+                (-1, -1), (-1, 3),
+                (-2, -1), (-2, 3),
+                (-3, -1), (-3, 0), (-3, 1), (-3, 2), (-3, 3)]
 
+        # 当2*2 噪点时
+        # for i in range(1, h - 1):
+        #     for j in range(1, w - 1):
+        #         sum_round = 0
+        #
+        #         try:
+        #             for m, n in func:
+        #                 if img[i + m, j + n] == 0:
+        #                     sum_round += 1
+        #             # print(sum_round)
+        #             if sum_round >= 11:
+        #                 img[i, j] = 0
+        #                 img[i, j + 1] = 0
+        #                 img[i - 1, j] = 0
+        #                 img[i - 1, j + 1] = 0
+        #                 # for m, n in func:
+        #                 #     img[i + m, j + n] = 0
+        #
+        #         except:
+        #             pass
+        # 3*3 噪点时
         for i in range(1, h - 1):
             for j in range(1, w - 1):
                 sum_round = 0
@@ -52,26 +68,37 @@ class PicBinary:
                         if img[i + m, j + n] == 0:
                             sum_round += 1
                     # print(sum_round)
-                    if sum_round >= 11:
+                    if sum_round >= 15:
                         img[i, j] = 0
                         img[i, j + 1] = 0
+                        img[i, j + 2] = 0
                         img[i - 1, j] = 0
                         img[i - 1, j + 1] = 0
+                        img[i - 1, j + 2] = 0
+                        img[i - 2, j] = 0
+                        img[i - 2, j + 1] = 0
+                        img[i - 2, j + 2] = 0
+
                         # for m, n in func:
                         #     img[i + m, j + n] = 0
+
                 except:
                     pass
         return img
 
     def get_begining(self, x):
         h, w = self.binaryed_img.shape
+        x += 3
         for j in range(x, w):
             for i in range(h):
-                if self.binaryed_img[i, j] == 255 and self.binaryed_img[i, j + 1] == 255 and self.binaryed_img[i, j + 2] == 255:
-                    if self.binaryed_img[i, j] not in self.visited:
-                        self.visited.add((i, j))
-                        self.q.put((i, j))
-                    break
+                try:
+                    if self.binaryed_img[i, j] == 255 and self.binaryed_img[i, j + 1] == 255 and self.binaryed_img[i, j + 2] == 255:
+                        if self.binaryed_img[i, j] not in self.visited:
+                            self.visited.add((i, j))
+                            self.q.put((i, j))
+                        break
+                except:
+                    pass
             if self.visited:
                 break
 
@@ -80,51 +107,86 @@ class PicBinary:
         :return:
         """
         h, w = self.binaryed_img.shape
-        print(h, w,"sss")
         func = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-        print(self.q.empty())
-        # print(self.q.get())
         while not self.q.empty():
             i, j = self.q.get()
-            print(i, j , "in while")
             for m, n in func:
-                print(i+m, j+n, "here")
                 try:
-                    if self.binaryed_img[i + m, j + n] == 255 and \
-                            self.binaryed_img not in self.visited:
-                        print(i, j, "for in ")
-                        # self.visited.add((i+m, j+n))
-                        # self.q.put(i + m, j + n)
+                    if self.binaryed_img[i + m, j + n] == 255 and (i + m, j + n) not in self.visited and i+m >= 0 and j + n >=0:
+                        # print(i+m, j+n, "in trasver")
+                        self.visited.add((i+m, j+n))
+                        self.q.put((i + m, j + n))
                 except Exception as e:
-                    print(e)
+                    # print(e)
                     pass
 
-
-
     def cut(self):
-        pass
+        img = np.zeros((60, 80), np.uint8)
+        x_max = 0
+        x_min = 200
+        for i, j in self.visited:
+            if j > x_max:
+                x_max = j
+            if j < x_min:
+                x_min= j
+        pixel_sum = 0
+        for i, j in self.visited:
+            if j- x_min + 10 > 79 or i > 59:
+                continue
+            img[i, j- x_min + 10] = 255
+            pixel_sum += 1
+        print(pixel_sum, "pixel_sum")
+        if pixel_sum < 100:
+            return "", "", x_max + 3
+        width = x_max - x_min
+        begin_index = self.i
+        self.i += 1
+        # save_name = f"./pic/dealed/{self.text[begin_index: self.i]}_{str(int(time.time()*1000))}.png"
+        if width > 60:
+            return "", "", "over"
+        if width > 35:
+            self.i += 1
+            # save_name = f"./pic/dealed2/{self.text[begin_index: self.i]}_{str(int(time.time()*1000))}.png"
+        elif width < 0:
+            return "", "", "over"
+        # cv.imwrite(save_name, img)
+        name = self.text[begin_index: self.i]
+        return x_max, name, img
 
     def main(self):
-        image = self.binary()
-        self.binaryed_img = self.delete_noisy(image)
-        # cv.imshow("ss", self.binaryed_img)
-        # cv.waitKey(0)
-        x = 0
-        for i in range(4):
-            self.get_begining(x)
-            self.traverse()
-        cv.imshow("img", self.visited)
-        cv.waitKey(0)
-
-
-# cv.imshow("sso", self.img)
-# cv.imshow("img", img)
-# cv.waitKey(0)
+        while True:
+            self.visited = set()
+            self.text, self.img = gen_captcha_text_and_image()
+            image = self.binary()
+            self.binaryed_img = self.delete_noisy(image)
+            x = 0
+            for i in range(4):
+                self.visited = set()
+                self.get_begining(x)
+                self.traverse()
+                x, name, img = self.cut()
+                print(name)
+                yield name, img
+                if not x:
+                    break
+                if img == "over":
+                    break
+                elif img == "noisy":
+                    pass
+            self.i = 0
+            self.visited = set()
 
 
 if __name__ == '__main__':
-    # text, img = gen_captcha_text_and_image()
-    img = cv.imread("./pic/agnu.png")
-    # cv.imshow("imgo", img)
-    # cv.waitKey(0)
-    PicBinary(img).main()
+    ac = PicBinary().main()
+    for i in range(40):
+        print(i, "ssssssssssssssssssssssssssssssssssssssssssss")
+        name, img = ac.__next__()
+        if not name:
+            continue
+        save_name = f"./pic/dealed/{name}_{str(int(time.time()*1000))}.png"
+        cv.imwrite(save_name, img)
+        # cv.imshow("img", img)
+        # cv.waitKey(0)
+        # cv.destroyAllWindows()
+
